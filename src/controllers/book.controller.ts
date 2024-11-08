aad2794c2fc910460c381da35cae94ff6d8915e1
imnotjs <134348800+imnotjs@users.noreply.github.com>
2024-11-08 14:26:38 +0700

    fix: response format

--- src/controllers/book.controller.ts
+++ src/controllers/book.controller.ts
@@ -1,77 +1,83 @@
-import type { Request, Response } from 'express';
-import BookService from '../services/book.service';
+import type { Request, Response } from "express";
+import BookService from "../services/book.service";
 
 export class BookController {
-  async addBook(req: Request, res: Response) {
-    try { 
-      const book = await BookService.addBook(req.body);
-      res.status(201).json(book);
-    } catch (error) {
-      if (error instanceof Error) {
-        res.status(400).json({ message: error.message });
-      } else {
-        res.status(400).json({ message: 'Unknown error' });
-      }
-    }
-  }
+	async addBook(req: Request, res: Response) {
+		try {
+			const book = await BookService.addBook(req.body);
 
-  async getAllBooks(req: Request, res: Response) {
-    try {
-      const books = await BookService.getAllBooks();
-      res.json(books);
-    } catch (error: any) {
-      if (error instanceof Error) {
-        res.status(400).json({ message: error.message });
-      } else {
-        res.status(400).json({ message: 'Unknown error' });
-      }
-    }
-  }
+			// res.status(201).json(book);
 
-  async getBookById(req: Request, res: Response) {
-    try {
-      const book = await BookService.getBookById(req.params.id);
-      if (!book) {
-        res.status(404).json({ message: 'Book not found' });
-        return;
-      }
-      res.json(book);
-    } catch (error) {
-      if (error instanceof Error) {
-        res.status(500).json({ message: error.message });
-      } else {
-        res.status(500).json({ message: 'Unknown error' });
-      }
-    }
-  }
+			// sama aja kaya sblmnya, tp ini sesuai response formatnya
+			res.status(201).json({ status: "success", message: "Successfully add book", data: book });
+		} catch (error) {
+			if (error instanceof Error) {
+				res.status(400).json({ status: "failed", message: error.message });
+			} else {
+				res.status(400).json({ status: "error", message: "Unknown error" });
+			}
+		}
+	}
 
-  async modifyBook(req: Request, res: Response) {
-    try {
-        const book = await BookService.modifyBook(req.params.id, req.body);
-        res.json(book);
-    } catch (error) {
-        if (error instanceof Error && error.message.includes('Invalid book ID format')) {
-            res.status(400).json({ message: error.message }); 
-        } else if (error instanceof Error && error.message.includes('not found')) {
-            res.status(404).json({ message: error.message });
-        } else {
-            res.status(500).json({ message: 'Internal server error' }); 
-        }
-    }
-  }
+	async getAllBooks(req: Request, res: Response) {
+		try {
+			const books = await BookService.getAllBooks();
+			res.status(201).json({ status: "success", message: "Successfully get all books", data: books });
+		} catch (error: any) {
+			if (error instanceof Error) {
+				res.status(400).json({ status: "failed", message: error.message });
+			} else {
+				res.status(400).json({ status: "error", message: "Unknown error" });
+			}
+		}
+	}
 
-  async removeBook(req: Request, res: Response) {
-    try {
-        const book = await BookService.removeBook(req.params.id);
-        res.json({ message: 'Book deleted successfully', book });
-    } catch (error) {
-        if (error instanceof Error && error.message.includes('Invalid book ID format')) {
-            res.status(400).json({ message: error.message });
-        } else if (error instanceof Error && error.message.includes('not found')) {
-            res.status(404).json({ message: error.message });
-        } else {
-            res.status(500).json({ message: 'Internal server error' });
-        }
-    }
-  }
+	async getBookById(req: Request, res: Response) {
+		try {
+			const book = await BookService.getBookById(req.params.id);
+			if (!book) {
+				res.status(404).json({ message: "Book not found" });
+				return;
+			}
+
+			res.status(201).json({ status: "success", message: "Successfully get book by ID", data: book });
+		} catch (error) {
+			if (error instanceof Error) {
+				res.status(500).json({ status: "failed", message: error.message });
+			} else {
+				res.status(500).json({ status: "error", message: "Unknown error" });
+			}
+		}
+	}
+
+	async modifyBook(req: Request, res: Response) {
+		try {
+			const book = await BookService.modifyBook(req.params.id, req.body);
+
+			res.status(201).json({ status: "success", message: "Successfully modify book by ID", data: book });
+		} catch (error) {
+			if (error instanceof Error && error.message.includes("Invalid book ID format")) {
+				res.status(400).json({ status: "failed", message: error.message });
+			} else if (error instanceof Error && error.message.includes("not found")) {
+				res.status(404).json({ status: "failed", message: error.message });
+			} else {
+				res.status(500).json({ status: "error", message: "Internal server error" });
+			}
+		}
+	}
+
+	async removeBook(req: Request, res: Response) {
+		try {
+			const book = await BookService.removeBook(req.params.id);
+			res.json({ status: "success", message: "Book deleted successfully" });
+		} catch (error) {
+			if (error instanceof Error && error.message.includes("Invalid book ID format")) {
+				res.status(400).json({ status: "failed", message: error.message });
+			} else if (error instanceof Error && error.message.includes("not found")) {
+				res.status(404).json({ status: "failed", message: error.message });
+			} else {
+				res.status(500).json({ status: "error", message: "Internal server error" });
+			}
+		}
+	}
 }
